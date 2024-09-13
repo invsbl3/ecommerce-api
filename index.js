38ad56f3c5fc053f0878dc0946ded911522c1181
unknown <luankevinfmb@gmail.com>
2024-09-13 02:21:29 -0400

    cors changes

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ const User = require("./models/user.model.js");
 
 // MIDDLEWARE
 app.use(express.json());
-app.use('*', cors());
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 // ROUTES
 app.use("/api/product", productRoute);
